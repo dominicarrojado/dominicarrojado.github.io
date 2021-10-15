556610a9858b31a2499a9710f51fdb180fc15f40
Dominic Arrojado <dominicarrojado@gmail.com>
2021-10-15 15:29:23 +0800

    include projects in sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,115 @@
 module.exports = {
   siteUrl: 'https://dominicarrojado.com',
   generateRobotsTxt: true,
+  additionalPaths: async (config) => [
+    // axa edms
+    await config.transform(config, '/axa-smart-travel-edm/'),
+    await config.transform(config, '/axa-smart-travel-2-edm/'),
+    await config.transform(config, '/axa-mothers-day-edm/'),
+    await config.transform(config, '/axa-shield-edm/'),
+    await config.transform(config, '/axa-mothers-day-edm/'),
+    await config.transform(config, '/axa-shield-edm/'),
+    await config.transform(config, '/axa-smart-home-edm/'),
+    await config.transform(config, '/axa-singtel-edm/'),
+    await config.transform(config, '/axa-september-promo-edm/'),
+
+    // m1 edms
+    await config.transform(config, '/m1-data-passport-edm/'),
+    await config.transform(config, '/m1-data-passport-2-edm/'),
+    await config.transform(config, '/m1-data-passport-3-edm/'),
+    await config.transform(config, '/m1-data-passport-4-edm/'),
+
+    // hashtag interactive edms
+    await config.transform(config, '/hashtag-interactive-welcome-edm/'),
+
+    // hashtag interactive apps
+    await config.transform(config, '/hashtag-interactive-christmas-game/'),
+    await config.transform(
+      config,
+      '/hashtag-interactive-valentines-day-card-app/'
+    ),
+
+    // hashtag interactive website
+    await config.transform(config, '/hashtag-interactive-website/'),
+    await config.transform(
+      config,
+      '/hashtag-interactive-website/blog-single.html'
+    ),
+    await config.transform(config, '/hashtag-interactive-website/blog.html'),
+    await config.transform(
+      config,
+      '/hashtag-interactive-website/branding.html'
+    ),
+    await config.transform(config, '/hashtag-interactive-website/careers.html'),
+    await config.transform(
+      config,
+      '/hashtag-interactive-website/contact-us.html'
+    ),
+    await config.transform(
+      config,
+      '/hashtag-interactive-website/content-marketing.html'
+    ),
+    await config.transform(
+      config,
+      '/hashtag-interactive-website/growth-hacking.html'
+    ),
+    await config.transform(
+      config,
+      '/hashtag-interactive-website/internship.html'
+    ),
+    await config.transform(
+      config,
+      '/hashtag-interactive-website/our-story.html'
+    ),
+    await config.transform(
+      config,
+      '/hashtag-interactive-website/privacy-policy.html'
+    ),
+    await config.transform(
+      config,
+      '/hashtag-interactive-website/research-and-analytics.html'
+    ),
+    await config.transform(
+      config,
+      '/hashtag-interactive-website/social-media-marketing.html'
+    ),
+    await config.transform(
+      config,
+      '/hashtag-interactive-website/strategic-online-media-and-planning.html'
+    ),
+    await config.transform(
+      config,
+      '/hashtag-interactive-website/strategic-planning.html'
+    ),
+    await config.transform(
+      config,
+      '/hashtag-interactive-website/web-and-interactive.html'
+    ),
+    await config.transform(
+      config,
+      '/hashtag-interactive-website/what-we-do.html'
+    ),
+
+    // kronenbourg website
+    await config.transform(config, '/kronenbourg-website/'),
+    await config.transform(config, '/kronenbourg-website/privacy-policy.html'),
+    await config.transform(
+      config,
+      '/kronenbourg-website/smoobar-contest-terms-and-conditions.html'
+    ),
+    await config.transform(config, '/kronenbourg-website/subscribe.html'),
+    await config.transform(config, '/kronenbourg-website/terms-of-use.html'),
+
+    // maybank website
+    await config.transform(config, '/maybank-fc-barcelona-website/'),
+
+    // qwerk website
+    await config.transform(config, '/qwerk-website/'),
+    await config.transform(config, '/qwerk-website/about.html'),
+    await config.transform(config, '/qwerk-website/contact-us.html'),
+    await config.transform(config, '/qwerk-website/faq.html'),
+
+    // razer apps
+    await config.transform(config, '/mouse-accuracy-game/'),
+  ],
 };
