6a21d589a9779b049a7525071c12399aaa4c4264
dominicarrojado <dominicarrojado@gmail.com>
2022-01-21 02:01:31 +0800

    update privacy policy

--- components/adUnit.tsx
+++ components/adUnit.tsx
@@ -19,7 +19,7 @@ function AdUnit({ className }: Props) {
         crossOrigin="anonymous"
       />
       <ins
-        className="adsbygoogle"
+        className="adsbygoogle block"
         data-ad-client="ca-pub-3632473845121107"
         data-ad-slot="4984498713"
         data-ad-format="auto"
