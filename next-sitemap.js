a3940682a33f892c5fe22fde5f0d95a684758f10
Dominic Arrojado <dominicarrojado@gmail.com>
2022-03-02 21:51:10 +0800

    add pages with .html in sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,51 +1,147 @@
 module.exports = {
   siteUrl: 'https://dominicarrojado.com',
   generateRobotsTxt: true,
-  additionalPaths: async (config) => [
-    // axa edms
-    await config.transform(config, '/axa-smart-travel-edm/'),
-    await config.transform(config, '/axa-smart-travel-2-edm/'),
-    await config.transform(config, '/axa-mothers-day-edm/'),
-    await config.transform(config, '/axa-shield-edm/'),
-    await config.transform(config, '/axa-smart-home-edm/'),
-    await config.transform(config, '/axa-singtel-edm/'),
-    await config.transform(config, '/axa-september-promo-edm/'),
+  additionalPaths: async (config) => {
+    const configNoTrailingSlash = {
+      ...config,
+      trailingSlash: false,
+    };
 
-    // m1 edms
-    await config.transform(config, '/m1-data-passport-edm/'),
-    await config.transform(config, '/m1-data-passport-2-edm/'),
-    await config.transform(config, '/m1-data-passport-3-edm/'),
-    await config.transform(config, '/m1-data-passport-4-edm/'),
+    return [
+      // axa edms
+      await config.transform(config, '/axa-smart-travel-edm/'),
+      await config.transform(config, '/axa-smart-travel-2-edm/'),
+      await config.transform(config, '/axa-mothers-day-edm/'),
+      await config.transform(config, '/axa-shield-edm/'),
+      await config.transform(config, '/axa-smart-home-edm/'),
+      await config.transform(config, '/axa-singtel-edm/'),
+      await config.transform(config, '/axa-september-promo-edm/'),
 
-    // hashtag interactive edms
-    await config.transform(config, '/hashtag-interactive-welcome-edm/'),
+      // m1 edms
+      await config.transform(config, '/m1-data-passport-edm/'),
+      await config.transform(config, '/m1-data-passport-2-edm/'),
+      await config.transform(config, '/m1-data-passport-3-edm/'),
+      await config.transform(config, '/m1-data-passport-4-edm/'),
 
-    // hashtag interactive apps
-    await config.transform(config, '/hashtag-interactive-christmas-game/'),
-    await config.transform(
-      config,
-      '/hashtag-interactive-valentines-day-card-app/'
-    ),
+      // hashtag interactive edms
+      await config.transform(config, '/hashtag-interactive-welcome-edm/'),
 
-    // hashtag interactive website
-    await config.transform(config, '/hashtag-interactive-website/'),
+      // hashtag interactive apps
+      await config.transform(config, '/hashtag-interactive-christmas-game/'),
+      await config.transform(
+        config,
+        '/hashtag-interactive-valentines-day-card-app/'
+      ),
 
-    // kronenbourg website
-    await config.transform(config, '/kronenbourg-website/'),
+      // hashtag interactive website
+      await config.transform(config, '/hashtag-interactive-website/'),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/blog-single.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/blog.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/branding.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/careers.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/contact-us.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/content-marketing.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/growth-hacking.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/internship.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/our-story.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/privacy-policy.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/research-and-analytics.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/social-media-marketing.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/strategic-online-media-and-planning.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/strategic-planning.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/web-and-interactive.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/hashtag-interactive-website/what-we-do.html'
+      ),
 
-    // maybank website
-    await config.transform(config, '/maybank-fc-barcelona-website/'),
+      // kronenbourg website
+      await config.transform(config, '/kronenbourg-website/'),
+      await config.transform(
+        configNoTrailingSlash,
+        '/kronenbourg-website/privacy-policy.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/kronenbourg-website/smoobar-contest-terms-and-conditions.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/kronenbourg-website/subscribe.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/kronenbourg-website/terms-of-use.html'
+      ),
 
-    // qwerk website
-    await config.transform(config, '/qwerk-website/'),
+      // maybank website
+      await config.transform(config, '/maybank-fc-barcelona-website/'),
 
-    // razer apps
-    await config.transform(config, '/mouse-accuracy-game/'),
+      // qwerk website
+      await config.transform(config, '/qwerk-website/'),
+      await config.transform(
+        configNoTrailingSlash,
+        '/qwerk-website/about.html'
+      ),
+      await config.transform(
+        configNoTrailingSlash,
+        '/qwerk-website/contact-us.html'
+      ),
+      await config.transform(configNoTrailingSlash, '/qwerk-website/faq.html'),
 
-    // blog examples
-    await config.transform(config, '/mjml-sample-edm/'),
-    await config.transform(config, '/react-typescript-accordion/'),
-    await config.transform(config, '/react-typescript-api-hooks/'),
-    await config.transform(config, '/react-typescript-swiper/'),
-  ],
+      // razer apps
+      await config.transform(config, '/mouse-accuracy-game/'),
+
+      // blog examples
+      await config.transform(config, '/mjml-sample-edm/'),
+      await config.transform(config, '/react-typescript-accordion/'),
+      await config.transform(config, '/react-typescript-api-hooks/'),
+      await config.transform(config, '/react-typescript-swiper/'),
+    ];
+  },
 };
