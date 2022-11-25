fb1320690f05f2a1f1c2df27446a239526b24985
Dominic Arrojado <dominicarrojado@gmail.com>
2022-11-25 17:13:34 +0800

    simplify NextLink component to just check if it starts with /posts

--- components/nextLink.tsx
+++ components/nextLink.tsx
@@ -1,34 +1,24 @@
 import Link, { LinkProps } from 'next/link';
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import { Route } from '../lib/types';
 
 export type Props = LinkProps & { href: string; children: ReactElement };
 
-export default function NextLink({ children, href, ...props }: Props) {
+export default function NextLink({
+  children,
+  href,
+  passHref,
+  ...props
+}: Props) {
   const child = React.Children.only(children);
-  const [shouldReload, setShouldReload] = useState(false);
 
-  const onMouseEnter = () => {
-    if (
-      !shouldReload &&
-      href.startsWith(Route.POSTS) &&
-      (window.adsbygoogle as any)?.loaded
-    ) {
-      setShouldReload(true);
-    }
-
-    if (typeof child.props.onMouseEnter === 'function') {
-      child.props.onMouseEnter();
-    }
-  };
-
-  if (shouldReload) {
-    return React.cloneElement(child, { href });
+  if (href.startsWith(Route.POSTS)) {
+    return React.cloneElement(child, { ...props, href });
   }
 
   return (
-    <Link {...props} href={href}>
-      {React.cloneElement(child, { onMouseEnter })}
+    <Link href={href} passHref={passHref}>
+      {React.cloneElement(child, props)}
     </Link>
   );
 }
