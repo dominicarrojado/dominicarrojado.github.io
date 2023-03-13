fa3dec7f4a1388864491cc8ed49154eb6cb10dae
Dominic Arrojado <dominicarrojado@gmail.com>
2023-03-14 00:15:47 +0800

    write test for InputGroup component

---
+++ components/__tests__/inputGroup.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import { getFakeSentence } from '../../lib/test-helpers';
+import InputGroup, { Props } from '../inputGroup';
+
+describe('<InputGroup />', () => {
+  const renderComponent = ({ children }: Props) =>
+    render(<InputGroup>{children}</InputGroup>);
+
+  it('should render children', () => {
+    const text = getFakeSentence();
+
+    renderComponent({ children: text });
+
+    const el = screen.queryByText(text);
+
+    expect(el).toBeInTheDocument();
+  });
+});
