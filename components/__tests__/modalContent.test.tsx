f1b9239cec03c38a6f251c0fe26bbb00a1a8e874
Dominic Arrojado <dominicarrojado@gmail.com>
2023-03-14 21:13:52 +0800

    write test for ModalContent component

---
+++ components/__tests__/modalContent.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import { getFakeSentence } from '../../lib/test-helpers';
+import ModalContent, { Props } from '../modalContent';
+
+describe('<ModalContent />', () => {
+  const renderComponent = ({ children }: Props) =>
+    render(<ModalContent>{children}</ModalContent>);
+
+  it('should render children', () => {
+    const text = getFakeSentence();
+
+    renderComponent({ children: text });
+
+    const el = screen.queryByText(text);
+
+    expect(el).toBeInTheDocument();
+  });
+});
