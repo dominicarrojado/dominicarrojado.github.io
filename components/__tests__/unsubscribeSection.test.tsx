19f27fc2dfaa977f2e9f6f4fba3d55f05396c9ca
Dominic Arrojado <dominicarrojado@gmail.com>
2023-03-29 10:34:16 +0800

    write tests for UnsubscribeSection component

---
+++ components/__tests__/unsubscribeSection.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { act, render } from '@testing-library/react';
+import * as UnsubscribeForm from '../unsubscribeForm';
+import * as UnsubscribeSuccess from '../unsubscribeSuccess';
+import UnsubscribeSection from '../unsubscribeSection';
+
+describe('<UnsubscribeSection />', () => {
+  const renderComponent = () => render(<UnsubscribeSection />);
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should render form component by default', () => {
+    const unsubscribeFormSpy = jest.spyOn(UnsubscribeForm, 'default');
+    const unsubscribeSuccessSpy = jest.spyOn(UnsubscribeSuccess, 'default');
+
+    renderComponent();
+
+    expect(unsubscribeFormSpy).toBeCalledTimes(1);
+    expect(unsubscribeSuccessSpy).not.toBeCalled();
+  });
+
+  it('should render success component on success', () => {
+    let onSuccessFunc = () => {};
+
+    const unsubscribeFormMock = jest
+      .spyOn(UnsubscribeForm, 'default')
+      .mockImplementation(({ onSuccess }) => {
+        onSuccessFunc = onSuccess;
+        return <></>;
+      });
+
+    const unsubscribeSuccessSpy = jest.spyOn(UnsubscribeSuccess, 'default');
+
+    renderComponent();
+
+    unsubscribeFormMock.mockClear();
+
+    act(() => {
+      onSuccessFunc();
+    });
+
+    expect(unsubscribeFormMock).not.toBeCalled();
+    expect(unsubscribeSuccessSpy).toBeCalledTimes(1);
+  });
+});
