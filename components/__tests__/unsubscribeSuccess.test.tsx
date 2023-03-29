67da919077c4a05c56fbb41a425f599c67b7b04c
Dominic Arrojado <dominicarrojado@gmail.com>
2023-03-29 10:42:35 +0800

    write tests for UnsubscribeSuccess component

---
+++ components/__tests__/unsubscribeSuccess.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react';
+import * as customHooks from '../../lib/custom-hooks';
+import UnsubscribeSuccess from '../unsubscribeSuccess';
+
+describe('<UnsubscribeSuccess />', () => {
+  const renderComponent = () => render(<UnsubscribeSuccess />);
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  describe('content', () => {
+    beforeEach(() => {
+      jest.spyOn(customHooks, 'useMounted').mockReturnValue(true);
+
+      renderComponent();
+    });
+
+    it('should have expected title', () => {
+      const titleEl = screen.queryByText('Unsubscribed');
+
+      expect(titleEl?.tagName).toBe('H5');
+    });
+
+    it('should have expected description', () => {
+      const titleEl = screen.queryByText(
+        "You have been successfully unsubscribed from my tech blog. I'm sorry to see you go, but I appreciate your time and interest in my content. If you change your mind and would like to resubscribe in the future, please don't hesitate to do so. Thank you!"
+      );
+
+      expect(titleEl?.tagName).toBe('P');
+    });
+  });
+});
