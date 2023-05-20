76a566ef3969d4da5aa96d89abefb87beccdfe0d
Dominic Arrojado <dominicarrojado@gmail.com>
2023-05-20 11:11:07 +0800

    fix button loader style

--- components/buttonLoader.tsx
+++ components/buttonLoader.tsx
@@ -7,6 +7,7 @@ export default function ButtonLoader() {
     <SvgLoader
       className={cn(
         'animate-spin -ml-1 mr-3 w-3.5 h-3.5 text-white',
+        'dark:text-black',
         'sm:w-4 sm:h-4',
         'xl:w-5 xl:h-5'
       )}
